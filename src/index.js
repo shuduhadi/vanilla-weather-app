a23db4487cff2e5a7baccae849afdddd7c67d451
shuduhadi <shuduhadi18@gmail.com>
2026-07-06 13:58:59 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 function displayWeatherCondition(response) {
     let temperatureElement = document.querySelector("#temperature");
-    let city = document.querySelector("#city");
     let temperature = response.data.temperature.current;
-    
+    let city = document.querySelector("#city");
+    let description = document.querySelector("#details");
+    let humidity = document.querySelector("#humdity");
+    let windSpeed = document.querySelector("#wind-speed");
+    let time = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+
+    time.innerHTML = formatDate(date);
     city.innerHTML = response.data.city;
+    description.innerHTML = response.data.condition.description;
+    humidity.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeed.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+};
+
 
 
 function searchCity(city) {
